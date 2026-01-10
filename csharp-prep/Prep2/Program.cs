a932bcf616668c63aba8d15ad8bf9b533046b1fe
EnochRomero <rom24032@byui.edu>
2026-01-10 11:30:49 -0700

    Finished proects 1 and 2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string grade = "";
+
+        Console.Write("Enter score percent to get grade: ");
+        string stringScore = Console.ReadLine();
+        int intScore = int.Parse(stringScore);
+
+        if (intScore >= 90)
+        {
+            grade = "A";
+        }
+
+        else if (intScore >= 80)
+        {
+            grade = "B";
+        }
+
+        else if (intScore >= 70)
+        {
+            grade = "C";
+        }
+
+        else if (intScore >= 60)
+        {
+            grade = "D";
+        }
+
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"The grade is {grade}");
+
+        if (intScore >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You Failed");
+        }
+
     }
 }
