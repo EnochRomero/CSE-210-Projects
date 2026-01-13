f3f58d06a7e47deb71f0791a4f38774ac865e0fa
EnochRomero <rom24032@byui.edu>
2026-01-13 15:51:13 -0700

    Added project 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Formats.Asn1;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int guess = -1;
+
+        List<int> numbers = new List<int>();
+
+        while (guess != 0)
+        {
+            Console.Write("Enter a number: ");
+
+            guess = int.Parse(Console.ReadLine());
+
+            numbers.Add(guess);
+        }
+
+        Console.WriteLine($"The sum of the numbers is: {numbers.Sum()} ");
+
+        Console.WriteLine($"The highest number is: {numbers.Max()}");
+
+        Console.WriteLine($"The average of all numbers entered is: {numbers.Average()}");
+
     }
 }
