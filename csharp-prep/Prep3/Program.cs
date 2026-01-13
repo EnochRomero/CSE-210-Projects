652ac0d254ccf162eb03fc87bf38180488c66d9d
EnochRomero <rom24032@byui.edu>
2026-01-13 15:15:28 -0700

    Number guessing game added

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomNumberGenerator = new Random();
+
+        int magicNumber = randomNumberGenerator.Next(1,101);
+
+        int guess = -10000;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine ("What is the magic number?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Your guess is too high!");
+            }
+
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Your guess is too low!");
+            }
+
+            else if (guess == magicNumber)
+            {
+                Console.WriteLine("You got it!");
+            }
+
+        }
+
+
+
     }
 }
