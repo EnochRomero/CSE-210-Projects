ee74a9db9bc544fa2566f738fc10d011285270dc
EnochRomero <rom24032@byui.edu>
2026-02-14 20:13:32 -0700

    Verse Project

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,49 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+
+        string choice = "";
+
+        Verse verse1 = new Verse();
+
+
+        int wordCount = verse1.GetVerseCount();
+        int wordsLeft = wordCount;
+
+    
+        while (choice != "quit")
+        {
+
+
+
+            Console.Clear();
+
+            verse1.DisplayVerse();
+
+            Console.WriteLine();
+            
+
+            
+            Console.WriteLine($"(You Have {wordsLeft} words left) PRESS ENTER TO REMOVE MORE WORDS OR TYPE OUT QUIT TO END THE PROGRAM");
+
+            choice = Console.ReadLine();
+
+            verse1.HideRandomWords();
+
+            if (wordsLeft <= 0) {break;}
+
+            wordsLeft -= 3;
+
+            if (wordsLeft < 0) {wordsLeft = 0;}
+
+
+
+
+        }
+
     }
 }
